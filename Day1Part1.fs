5a067386df1a129828bb6cf9a0612ad25afd9548
Erik Baalhuis <ebbaalhuis@gmail.com>
2024-12-02 22:32:23 +0100

    Set up some tests

--- Day1Part1.fs
+++ Day1Part1.fs
@@ -1,17 +1,15 @@
-﻿module Day1Part1
+﻿namespace Advent2024
+    module Day1Part1 =
+        let readLines filePath = System.IO.File.ReadLines(filePath);
 
-let readLines filePath = System.IO.File.ReadLines(filePath);
+        let lines = readLines "C:\\Users\\Erik\\source\\repos\\Advent2024\\input1.txt"
 
-let lines = readLines "C:\\Users\\Erik\\source\\repos\\Advent2024\\input1.txt"
+        let takeLeft (line: string) = line.Split ' ' |> Array.head
+        let takeRight (line: string) = line.Split ' ' |> Array.last
 
-let takeLeft (line: string) = line.Split ' ' |> Array.head
-let takeRight (line: string) = line.Split ' ' |> Array.last
+        let a = List.ofSeq(lines) |> List.map(takeLeft) |> List.sort |> List.map int
+        let b = List.ofSeq(lines) |> List.map(takeRight) |> List.sort |> List.map int
 
-let a = List.ofSeq(lines) |> List.map(takeLeft) |> List.sort |> List.map int
-let b = List.ofSeq(lines) |> List.map(takeRight) |> List.sort |> List.map int
+        let diffs = List.map2 (fun x y -> abs(x - y)) a b
 
-let diffs = List.map2 (fun x y -> abs(x - y)) a b
-
-let sum = List.reduce (fun x y -> x + y) diffs
-
-printfn "%d" sum
+        let result = List.reduce (fun x y -> x + y) diffs
--- Day1Part2.fs
+++ Day1Part2.fs
@@ -1,24 +1,22 @@
-﻿module Day1Part2
+﻿namespace Advent2024
+    module Day1Part2 =
+        let readLines filePath = System.IO.File.ReadLines(filePath);
 
-let readLines filePath = System.IO.File.ReadLines(filePath);
+        let lines = readLines "C:\\Users\\Erik\\source\\repos\\Advent2024\\input1.txt"
 
-let lines = readLines "C:\\Users\\Erik\\source\\repos\\Advent2024\\input1.txt"
+        let takeLeft (line: string) = line.Split ' ' |> Array.head
+        let takeRight (line: string) = line.Split ' ' |> Array.last
 
-let takeLeft (line: string) = line.Split ' ' |> Array.head
-let takeRight (line: string) = line.Split ' ' |> Array.last
+        let a = List.ofSeq(lines) |> List.map(takeLeft) |> List.map int
+        let b = List.ofSeq(lines) |> List.map(takeRight) |> List.map int
 
-let a = List.ofSeq(lines) |> List.map(takeLeft) |> List.map int
-let b = List.ofSeq(lines) |> List.map(takeRight) |> List.map int
+        let counts = List.countBy id b
+        let z = a |> List.map (fun x -> List.filter(fun y -> fst y = x) counts) 
+        let z2 = z |> List.map (fun x -> 
+            if x.IsEmpty then
+                0
+            else
+                List.head x |> snd)
 
-let counts = List.countBy id b
-let z = a |> List.map (fun x -> List.filter(fun y -> fst y = x) counts) 
-let z2 = z |> List.map (fun x -> 
-    if x.IsEmpty then
-        0
-    else
-        List.head x |> snd)
-
-let scores = List.map2 (fun x y -> x * y) a z2
-let sum = List.reduce (fun x y -> x + y) scores
-
-printfn "%d" sum
+        let scores = List.map2 (fun x y -> x * y) a z2
+        let result = List.reduce (fun x y -> x + y) scores
