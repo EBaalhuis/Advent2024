839ea7ac1da3456e6f8383fe31b877114d44d79c
Erik Baalhuis <ebbaalhuis@gmail.com>
2024-12-07 20:54:23 +0100

    Make day 4 solutions faster by doing fewer data transformations

--- Day4Part1.fs
+++ Day4Part1.fs
@@ -4,23 +4,18 @@ open System
     module Day4Part1 =
         let readLines filePath = System.IO.File.ReadLines(filePath);
 
-        let lines = readLines "input4.txt" |> List.ofSeq 
-
-        let get_element_at (x: int) (y: int) (lines: string seq) =
-            (Array.ofSeq lines)[y] |>
-            Array.ofSeq |>
-            (fun line -> Array.get line x)
+        let lines = readLines "input4.txt" |> Array.ofSeq |> Array.map Array.ofSeq
 
         let is_xmas (start_x: int) (start_y: int) (delta_x: int) (delta_y: int) =
-            if start_x + 3 * delta_x < 0 || start_x + 3 * delta_x >= lines.Head.Length then
+            if start_x + 3 * delta_x < 0 || start_x + 3 * delta_x >= lines[0].Length then
                 false
             else if start_y + 3 * delta_y < 0 || start_y + 3 * delta_y >= lines.Length then
                 false
             else
-                get_element_at start_x start_y lines = 'X' &&
-                get_element_at (start_x + delta_x) (start_y + delta_y) lines = 'M' &&
-                get_element_at (start_x + 2 * delta_x) (start_y + 2 * delta_y) lines = 'A' &&
-                get_element_at (start_x + 3 * delta_x) (start_y + 3 * delta_y) lines = 'S'
+                lines[start_y][start_x] = 'X' &&
+                lines[start_y + delta_y][start_x + delta_x] = 'M' &&
+                lines[start_y + 2 * delta_y][start_x + 2 * delta_x] = 'A' &&
+                lines[start_y + 3 * delta_y][start_x + 3 * delta_x] = 'S'
 
         let count_starting_at (start_x: int) (start_y: int) =
             (if (is_xmas start_x start_y 0 1) then 1 else 0) +
@@ -33,7 +28,7 @@ open System
             (if (is_xmas start_x start_y -1 -1) then 1 else 0)
 
         let result = 
-            Seq.allPairs [0 .. lines.Head.Length - 1] [0 .. lines.Length - 1] |>
+            Seq.allPairs [0 .. lines[0].Length - 1] [0 .. lines.Length - 1] |>
             Seq.map (fun pair -> 
                 count_starting_at (fst pair) (snd pair)) |>
             Seq.reduce (+)
--- Day4Part2.fs
+++ Day4Part2.fs
@@ -4,31 +4,26 @@ open System
     module Day4Part2 =
         let readLines filePath = System.IO.File.ReadLines(filePath);
 
-        let lines = readLines "input4.txt" |> List.ofSeq 
+        let lines = readLines "input4.txt" |> Array.ofSeq |> Array.map Array.ofSeq
 
-        let get_element_at (x: int) (y: int) (lines: string seq) =
-            (Array.ofSeq lines)[y] |>
-            Array.ofSeq |>
-            (fun line -> Array.get line x)
-
-        let is_mas (x: int) (y: int) (delta_x: int) (delta_y: int) (lines: string seq) =
-            get_element_at x y lines = 'A' &&
+        let is_mas (x: int) (y: int) (delta_x: int) (delta_y: int) =
+            lines[y][x] = 'A' &&
             (
-                (get_element_at (x - delta_x) (y - delta_y) lines = 'M' && get_element_at (x + delta_x) (y + delta_y) lines = 'S') ||
-                (get_element_at (x - delta_x) (y - delta_y) lines = 'S' && get_element_at (x + delta_x) (y + delta_y) lines = 'M')
+                (lines[y - delta_y][x - delta_x] = 'M' && lines[y + delta_y][x + delta_x] = 'S') ||
+                (lines[y - delta_y][x - delta_x] = 'S' && lines[y + delta_y][x + delta_x] = 'M')
             )
 
-        let diagonal1_mas (x: int) (y: int) (lines: string seq) =
-            is_mas x y 1 1 lines
+        let diagonal1_mas (x: int) (y: int) =
+            is_mas x y 1 1
 
-        let diagonal2_mas (x: int) (y: int) (lines: string seq) =
-            is_mas x y -1 1 lines
+        let diagonal2_mas (x: int) (y: int) =
+            is_mas x y -1 1
 
         let result = 
-            Seq.allPairs [1 .. lines.Head.Length - 2] [1 .. lines.Length - 2] |>
+            Seq.allPairs [1 .. lines[0].Length - 2] [1 .. lines.Length - 2] |>
             Seq.map (fun pair -> 
-                diagonal1_mas (fst pair) (snd pair) lines &&
-                diagonal2_mas (fst pair) (snd pair) lines) |>
+                diagonal1_mas (fst pair) (snd pair) &&
+                diagonal2_mas (fst pair) (snd pair)) |>
             Seq.filter id |>
             Seq.length
         
