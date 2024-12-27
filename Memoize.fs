86f342620d72a94c85ba143a6f79e1dcdf54f14b
Erik Baalhuis <ebbaalhuis@gmail.com>
2024-12-27 14:17:30 +0100

    Memoize module

---
+++ Memoize.fs
@@ -0,0 +1,13 @@
+﻿namespace Advent2024
+    module Memoize =
+    // jindraivanek.hashnode.dev
+
+        let memoize f =
+            let cache = System.Collections.Concurrent.ConcurrentDictionary()
+            fun x -> cache.GetOrAdd(x, lazy f x).Value
+
+        let memoizeRec f =
+            let cache = System.Collections.Concurrent.ConcurrentDictionary()
+            let rec recF x =
+                cache.GetOrAdd(x, lazy f recF x).Value
+            recF
